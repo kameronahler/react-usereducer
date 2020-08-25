13853d2663e06b4701035c072d61ef7b11d31b0f
Kameron Ahler <kamahler@gmail.com>
2020-08-25 12:53:22 -0600

    convert to component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,37 +1,10 @@
-import React, { useState, useReducer } from 'react'
-
-const REDUCER_ACTION = {
-  SUBTRACT: 'subtract',
-  ADD: 'add',
-}
-
-const stateReducer = (state, action) => {
-  switch (action.type) {
-    case REDUCER_ACTION.SUBTRACT:
-      return { count: state.count - 1 }
-    case REDUCER_ACTION.ADD:
-      return { count: state.count + 1 }
-  }
-}
+import React, { useReducer } from 'react'
+import Counter from '../Counter/Counter'
 
 export default function App() {
-  const [state, dispatch] = useReducer(stateReducer, { count: 0 })
-
-  const changeCount = (e) => {
-    const type = e.currentTarget.dataset.type
-
-    dispatch({ type: type })
-  }
-
   return (
-    <div>
-      <button data-type={REDUCER_ACTION.SUBTRACT} onClick={changeCount}>
-        -
-      </button>
-      <span>{state.count}</span>
-      <button data-type={REDUCER_ACTION.ADD} onClick={changeCount}>
-        +
-      </button>
-    </div>
+    <>
+      <Counter />
+    </>
   )
 }
