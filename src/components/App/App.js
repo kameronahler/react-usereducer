8dbe8c662301d006039991938913d359417c3206
Kameron Ahler <kamahler@gmail.com>
2020-08-25 11:54:30 -0600

    add exercise markup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function App() {
-  return <div></div>
+  const [count, setCount] = useState(() => {
+    return 0
+  })
+
+  const addCount = () => {
+    setCount((prev) => prev + 1)
+  }
+  const subtractCount = () => {
+    setCount((prev) => prev - 1)
+  }
+
+  return (
+    <div>
+      <button onClick={subtractCount}>-</button>
+      <span>{count}</span>
+      <button onClick={addCount}>+</button>
+    </div>
+  )
 }
