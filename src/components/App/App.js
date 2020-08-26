bb40651c68276731e4345240bc0fd4622b61fe02
Kameron Ahler <kamahler@gmail.com>
2020-08-25 19:28:09 -0600

    finally get 2nd reducer state going

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,13 @@
 import React, { useReducer, useState } from 'react'
 import Counter from '../Counter/Counter'
 
-// structured key for switch
 const REDUCER_ACTION_KEY = {
   ADD: 'Add Todo',
+  CHANGE: 'Delete Todo',
+  EDIT: 'Edit Todo',
 }
 
-// runs whenever dispatch is called
-const reducerActions = (state, action) => {
+const reducerActions = (items, action) => {
   const addNewItem = {
     id: Date.now(),
     complete: false,
@@ -16,31 +16,40 @@ const reducerActions = (state, action) => {
 
   switch (action.type) {
     case REDUCER_ACTION_KEY.ADD:
-      return [addNewItem, ...state]
+      return [addNewItem, ...items]
+    case REDUCER_ACTION_KEY.CHANGE:
+      return items.map((el) => {
+        if (el.id === action.payload.keyToDelete) {
+          return { ...el, complete: !el.complete }
+        } else {
+          return el
+        }
+      })
     default:
-      console.log('something went wrong')
+      console.log('error')
   }
 }
 
 export default function App() {
-  const [stagedItem, setStagedItem] = useState('') // local state initializer
-
-  // items refers to this particular useReducer's state
-  // dispatch refers to this particular useReducer's action(s) function
   const [items, dispatch] = useReducer(reducerActions, [])
 
-  // form onSubmit
+  const [stagedItem, setStagedItem] = useState('')
+
   const submit = (e) => {
     e.preventDefault()
-
-    // includes the type for the switch statement in the useReducer's function
-    // and a payload which is essentially rider-data, or a diy props for action
     dispatch({ type: REDUCER_ACTION_KEY.ADD, payload: { text: stagedItem } })
-
-    setStagedItem('') // clear the input
+    setStagedItem('')
   }
 
-  // render
+  const complete = (e) => {
+    const keyToDelete = parseInt(e.currentTarget.dataset.key)
+
+    dispatch({
+      type: REDUCER_ACTION_KEY.CHANGE,
+      payload: { keyToDelete: keyToDelete },
+    })
+  }
+
   return (
     <>
       <form onSubmit={submit}>
@@ -57,9 +66,22 @@ export default function App() {
       </form>
 
       <ul>
-        {items.map((el) => {
-          return <li key={el.id}>{el.text}</li>
-        })}
+        {items
+          .filter((el) => !el.complete)
+          .map((el) => {
+            return (
+              <li key={el.id}>
+                <span data-edit={el.id}>{el.text}</span>
+                <button
+                  data-key={el.id}
+                  onClick={complete}
+                  className='material-icons'
+                >
+                  check
+                </button>
+              </li>
+            )
+          })}
       </ul>
 
       <Counter />
